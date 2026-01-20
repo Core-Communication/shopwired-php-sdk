7709588a6c40affc53ff6ca97550bba7e403c47b
vtadmin <vtadmin@encryptedemail.info>
2026-01-20 10:33:28 +0530

    package uploaded

--- appoly/smart-schema/src/views/impl/hidden.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-$old_value = old($field['name'], isset($config['initial'][$field['name']]) ? $config['initial'][$field['name']] : '');
-?>
-
-<input name="{{ $field['name'] }}" type="{{ $field['type'] }}" id="{{ $field['name'] }}"
-       aria-describedby="{{ $field['name'] }}Help"
-       value="{{ $old_value }}">
